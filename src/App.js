3dd3caedede9d47cd8a349e45d4bb9998eb7b731
larrygf02 <raulgf_02@hotmail.com>
2019-11-02 16:07:00 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 function App() {
+  const [frase, obtenerFrase] = useState({})
+  
+  const consultarAPI = async () => {
+    const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    console.log(resultado.data[0]);
+    // agregar el resultado del api al state
+    obtenerFrase(resultado.data[0])
+  }
+  
+  //Consulta a una rest api por useEffect
+  useEffect(
+    () => {
+      consultarAPI()
+    }, [])
+
+  console.log(frase);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hola</h1>
   );
 }
 
