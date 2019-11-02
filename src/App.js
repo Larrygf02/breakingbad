6d7bec1011b9eac0b1ca1ec2a02f6a5557f2eea6
larrygf02 <raulgf_02@hotmail.com>
2019-11-02 16:20:15 -0500

    Generando frases

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
+function Frase({frase}) {
+  return (
+    <div className="frase">
+      <h1>{frase.quote}</h1>
+      <p>- {frase.author}</p>
+    </div>
+  )
+}
+
 function App() {
   const [frase, obtenerFrase] = useState({})
   
@@ -19,7 +28,10 @@ function App() {
 
   console.log(frase);
   return (
-    <h1>Hola</h1>
+    <div className="contenedor">
+      <Frase frase={frase}/>
+      <button onClick={consultarAPI}>Generar Nueva</button>
+    </div>
   );
 }
 
